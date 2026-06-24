4a55abd447a36a813c8f8febe18363ee442c1e75
Sahurkan <asurhalvazed93556@gmail.com>
2026-06-24 19:46:35 +0700

    mengubah nama

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pusat_pesantren"
+    namespace = "com.mohsahur.pusat_pesantren"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
